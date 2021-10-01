a52c583affb4c9a06504c3d24fdf8671954304b4
Benjamin Douglas <74840985+bentdoug@users.noreply.github.com>
2021-10-01 09:55:16 -0400

    Started ParkingGarage and ParkingGarageConfig

--- parkinggarage/ParkingGarage.java
+++ parkinggarage/ParkingGarage.java
@@ -11,8 +11,34 @@ package parkinggarage;
 public class ParkingGarage {
 
     /**
-     * @param args the command line arguments
+     * 
      */
+    private static int[] floorProfile;
+    private static int hourlyRate;
+    
+    /**
+     * Instantiates the specific parking garage
+     * @param floorProfile - an array of numFloors cells each of which holds a value
+     *      representing the total amount of parking spots on that respective floor
+     * @param hourlyRate - rate at which the customers get charged per hour of parking
+     */
+    public ParkingGarage(int[] floorProfile, int hourlyRate){
+        this.floorProfile = floorProfile;
+        this.hourlyRate = hourlyRate;
+    }
+    
+    @Override
+    public String toString(){
+        String ret = "";
+        String floorProfileString = "";
+        String hourlyRateString = "";
+        ret = "This garage has " + floorProfile.length + " floors.\n";
+        for(int x = 0; x < floorProfile.length; x++){
+            ret += "Floor " + (x+1) + " has " + floorProfile[x] + " spots. \n";
+        }
+        ret += "The hourly rate for parking here is $" + hourlyRate;
+        return ret;
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
