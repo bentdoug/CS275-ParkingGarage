9c0520d4666af4752db46973d443e450b3ed51da
ConnorFritsch <cofritsch@ursinus.edu>
2021-10-07 19:14:12 -0700

    Create databaseio.java

---
+++ parkinggarage/databaseio.java
@@ -0,0 +1,28 @@
+package parkinggarage;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+public class databaseio {
+
+    public void newCar(id, time) {
+        //stores the customer id and time they came in
+    }
+
+    public void getUsedIDs() {
+        //returns an array of id numbers that are currently available to be assigned to customers
+    }
+
+    public void getUnusedIDs() {
+        //returns an array of id numbers in use and id numbers not in use and rewrites them to the appropriate flat file
+    }
+
+    public void returnUsedUnusedIDs(used, unused) {
+        //recieves an updated array of id numbers in use and id numbers not in use and re-writes them to the
+        // appropriate flat file
+    }
+
+    public void getTimeIn(Id) {
+        //recieves the customers id and returns the time they entered the parking garage
+    }
+
+}
