6571718b74399219723d0777740f98963daef4ad
Ethan <etcuthbertson@ursinus.edu>
2021-11-17 17:41:59 -0500

    Added getParkingStatus.

--- parkinggarage/OpenSpots.java
+++ parkinggarage/OpenSpots.java
@@ -1,4 +1,11 @@
 package parkinggarage;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import parkinggarage.databaseio;
 
 /**
@@ -9,6 +16,7 @@ import parkinggarage.databaseio;
 
 public class OpenSpots{   //Doesn't need to extend. I could call the variable floorprofile from parkinggarage if I need it.
 
+    /*
     private int[] refreshOpenSpots()   //Should garageStatus be changed to databaseIO.getParkingStatus?
     {
         int[] garageStatus = databaseio.getParkingStatus();
@@ -18,5 +26,28 @@ public class OpenSpots{   //Doesn't need to extend. I could call the variable fl
             openSpots[i] = ParkingGarage.floorProfile[i] - garageStatus[i];   //If static status gets removed from floorProfile, this should be updated to this.floorProfile[i].
         }
         return openSpots;
-    }      
+    }*/
+    
+    public static boolean getParkingStatus() throws FileNotFoundException, IOException {
+    //Written by Ethan Cuthbertson
+    //Returns a boolean which represents wheter or not the parking garage is full
+        
+        String source = new File("").getAbsolutePath() + "\\src\\txtfiles\\database\\currentGarageStatus.txt";
+        FileInputStream inStream = new FileInputStream(source);
+        DataInputStream daStream = new DataInputStream(inStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(daStream));
+        int spotsTaken;
+        boolean isFull = true;
+        
+        int i = 0;      
+        while(i < ParkingGarage.floorProfile.length && isFull == false) {
+            spotsTaken = reader.read();
+            if(spotsTaken < ParkingGarage.floorProfile[i]) {
+                isFull = false;
+            }
+            i++;
+        }
+        daStream.close();
+        return isFull;
+    }
 }
