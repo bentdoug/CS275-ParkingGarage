99945c5358b57823f2eac9d96f681c83b7c2657b
Ethan <etcuthbertson@ursinus.edu>
2021-12-01 11:16:41 -0500

    It should theoretically work now. When testing my PSVM (using the alternative file sources which I have commented out because my file organization is messed up) I got 110 total open spots. That should fit.

--- parkinggarage/OpenSpots.java
+++ parkinggarage/OpenSpots.java
@@ -32,23 +32,30 @@ public class OpenSpots{   //Doesn't need to extend. I could call the variable fl
     //Written by Ethan Cuthbertson
     //Returns a boolean which represents wheter or not the parking garage is full
         
-        String source = new File("").getAbsolutePath() + "\\src\\txtfiles\\database\\currentGarageStatus.txt";
-        FileInputStream inStream = new FileInputStream(source);
-        DataInputStream daStream = new DataInputStream(inStream);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(daStream));
+        String source1 = new File("").getAbsolutePath() + "\\src\\txtfiles\\database\\currentGarageStatus.txt";
+        FileInputStream inStream1 = new FileInputStream(source1);
+        DataInputStream daStream1 = new DataInputStream(inStream1);
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(daStream1));
+        
+        String source2 = new File("").getAbsolutePath() + "\\src\\txtfiles\\database\\currentGarageStatus.txt";
+        FileInputStream inStream2 = new FileInputStream(source2);
+        DataInputStream daStream2 = new DataInputStream(inStream2);
+        BufferedReader reader2 = new BufferedReader(new InputStreamReader(daStream2));
+        
         int spotsTaken;
-        int numFloors = ParkingGarage.floorProfile[0];
+        int numFloors = Integer.parseInt(reader2.readLine());
         boolean isFull = true;
         
         int i = 1;      
         while(i <= numFloors && isFull == true) {
-            spotsTaken = Integer.parseInt(reader.readLine());
-            if(spotsTaken < ParkingGarage.floorProfile[i]) {
+            spotsTaken = Integer.parseInt(reader1.readLine());
+            if(spotsTaken < Integer.parseInt(reader2.readLine())) {
                 isFull = false;
             }
             i++;
         }
-        daStream.close();
+        daStream1.close();
+        daStream2.close();
         return isFull;
     }
     
@@ -56,27 +63,37 @@ public class OpenSpots{   //Doesn't need to extend. I could call the variable fl
     //Written by Ethan Cuthbertson
     //Returns an int which tell how many open spots are left in the whole parking garage
         
-        String source = new File("").getAbsolutePath() + "\\src\\txtfiles\\database\\currentGarageStatus.txt";
-        FileInputStream inStream = new FileInputStream(source);
-        DataInputStream daStream = new DataInputStream(inStream);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(daStream));
+        String source1 = new File("").getAbsolutePath() + "\\src\\txtfiles\\database\\currentGarageStatus.txt";
+        //THIS VERSION WORKS FOR ETHAN (Who has messed up package organization) String source1 = new File("").getAbsolutePath() + "\\src\\ParkingGarage\\CS275-ParkingGarage\\txtfiles\\database\\currentGarageStatus.txt";
+        FileInputStream inStream1 = new FileInputStream(source1);
+        DataInputStream daStream1 = new DataInputStream(inStream1);
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(daStream1));
+        
+        String source2 = new File("").getAbsolutePath() + "\\src\\txtfiles\\config\\floorProfiles.txt";
+        //THIS VERION WORKS FOR ETHAN (Who has messed up package organization) String source2 = new File("").getAbsolutePath() + "\\src\\ParkingGarage\\CS275-ParkingGarage\\txtfiles\\config\\floorProfiles.txt";
+        FileInputStream inStream2 = new FileInputStream(source2);
+        DataInputStream daStream2 = new DataInputStream(inStream2);
+        BufferedReader reader2 = new BufferedReader(new InputStreamReader(daStream2));
+        
         int spotsTaken;
-        int numFloors = ParkingGarage.floorProfile[0];
+        int numFloors = Integer.parseInt(reader2.readLine());
         int totalOpenSpots = 0;
             
         for(int i = 1; i <= numFloors; i++) {
-            spotsTaken = Integer.parseInt(reader.readLine());
-            if(spotsTaken < ParkingGarage.floorProfile[i]) {
-                totalOpenSpots += ParkingGarage.floorProfile[i] - spotsTaken;
+            spotsTaken = Integer.parseInt(reader1.readLine());
+            int curFloor = Integer.parseInt(reader2.readLine());
+            if(spotsTaken < curFloor) {
+                totalOpenSpots += curFloor - spotsTaken;
             }
         }
-        daStream.close();
+        daStream1.close();
+        daStream2.close();
         return totalOpenSpots;
     }
     
     // Do a PSVM and try to fix
     public static void main (String[] args) throws FileNotFoundException, IOException {
-        String source = new File("").getAbsolutePath() + "\\src\\ParkingGarage\\CS275-ParkingGarage\\txtfiles\\database\\currentGarageStatus.txt";
+        /*String source = new File("").getAbsolutePath() + "\\src\\ParkingGarage\\CS275-ParkingGarage\\txtfiles\\database\\currentGarageStatus.txt";
         System.out.println(source);
         FileInputStream inStream = new FileInputStream(source);
         DataInputStream daStream = new DataInputStream(inStream);
@@ -88,13 +105,14 @@ public class OpenSpots{   //Doesn't need to extend. I could call the variable fl
             int toOutput = Integer.parseInt(reader.readLine());
             System.out.println(toOutput);
         }
-        daStream.close();
+        daStream.close();*/
+        System.out.println(getNumOpenSpots());
     }
     
     /*
       11/21/21
       Ethan Cuthbertson: Fixed four glitches
-      1. In the code's body, there was improper capitilization, as parkinggarage was written as "ParkingGarage".
+      1. NO In the code's body, there was improper capitilization, as parkinggarage was written as "ParkingGarage".
       2. I mistook the first line of currentGarageStatus.txt as the line which stores the number of floors instead of the first line of floorProfiles.
       3. Given the above mistake, the use of indexes represented by "int i" was all mixed up and incorrect.
       4. By using the BufferedReader method "read()" I was getting incorrect numbers. I know use "readLine" then convert it from String to int.
